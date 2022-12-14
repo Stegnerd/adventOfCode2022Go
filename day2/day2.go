b00446a3555a54b49b2e362efda8437f8ddf4662
Dalton Stegner <dmstegner@gmail.com>
2022-12-13 22:53:13 -0500

    day 2 puzzle 2 done

--- day2/day2.go
+++ day2/day2.go
@@ -29,6 +29,11 @@ var (
 		"Y": Y, // paper
 		"Z": Z, // scissors
 	}
+	expectedResult = map[string]int{
+		"X": 0,
+		"Y": 3,
+		"Z": 6,
+	}
 )
 
 func Puzzle1() {
@@ -91,5 +96,64 @@ func matchResult(opponent, me int) int {
 }
 
 func Puzzle2() {
+	file, err := os.Open("data/day2puzzle1data.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			panic(err)
+		}
+	}(file)
 
+	result := 0
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		row := scanner.Text()
+		rowArr := strings.Split(row, " ")
+
+		opponentGuess := opponentGuesses[rowArr[0]]
+		expectedResult := expectedResult[rowArr[1]]
+
+		result += matchResult2(opponentGuess, expectedResult)
+	}
+
+	// not 11088
+	fmt.Printf("\n puzzle2 result: %d  \n", result)
+}
+
+func matchResult2(opponentGuess, expectedResult int) int {
+	score := 0
+	switch expectedResult {
+	// I lose
+	case 0:
+		if opponentGuess == A {
+			score += Z
+		} else if opponentGuess == B {
+			score += X
+		} else {
+			score += Y
+		}
+	case 3:
+		score += 3
+		if opponentGuess == A {
+			score += X
+		} else if opponentGuess == B {
+			score += Y
+		} else {
+			score += Z
+		}
+	case 6:
+		score += 6
+		if opponentGuess == A {
+			score += Y
+		} else if opponentGuess == B {
+			score += Z
+		} else {
+			score += X
+		}
+	}
+
+	return score
 }
