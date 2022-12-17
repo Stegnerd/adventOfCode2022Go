dc69919bfb6537d8228004b7af3df452b51a6650
Dalton Stegner <dmstegner@gmail.com>
2022-12-17 00:17:20 -0500

    day 4 puzzle 2 done

--- day4/day4.go
+++ day4/day4.go
@@ -4,8 +4,6 @@ import (
 	"adventOfCode2022/util"
 	"bufio"
 	"fmt"
-	"log"
-	"os"
 	"strconv"
 	"strings"
 )
@@ -48,16 +46,7 @@ func containCheck(range1, range2 []string) bool {
 }
 
 func Puzzle2() {
-	file, err := os.Open("data/day4puzzle1data.txt")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer func(file *os.File) {
-		err := file.Close()
-		if err != nil {
-			panic(err)
-		}
-	}(file)
+	file := util.ReadFile("day4puzzle1data.txt")
 
 	count := 0
 	scanner := bufio.NewScanner(file)
@@ -68,9 +57,54 @@ func Puzzle2() {
 		range1 := strings.Split(ranges[0], "-")
 		range2 := strings.Split(ranges[1], "-")
 
-		if containCheck(range1, range2) {
+		if containCheckPartial(range1, range2) {
 			count += 1
 		}
 	}
 
+	fmt.Printf("\n\n count: %d \n\n", count)
+}
+
+func containCheckPartial(range1, range2 []string) bool {
+	range1Start, _ := strconv.Atoi(range1[0])
+	range1End, _ := strconv.Atoi(range1[1])
+	range2Start, _ := strconv.Atoi(range2[0])
+	range2End, _ := strconv.Atoi(range2[1])
+
+	// total contains
+	if range1Start <= range2Start && range1End >= range2End {
+		return true
+	}
+
+	// total contains
+	if range2Start <= range1Start && range2End >= range1End {
+		return true
+	}
+
+	// partial
+	//	2-6,4-8 | 2 <= 4 | 2 <= 8
+	// r2 overlaps partial of r1
+	if range2Start <= range1Start && range1Start <= range2End {
+		return true
+	}
+
+	// 5-7,7-9 | 7 <= 7 | 7 <= 9
+	// r1 overlaps r2 in single section
+	if range2Start <= range1End && range1End <= range2End {
+		return true
+	}
+
+	// 2-8,3-7 | 2 <= 3 | 3 <= 8
+	// r1 overlaps partial of r2.
+	if range1Start <= range2Start && range2Start <= range1End {
+		return true
+	}
+
+	//  6-6,4-6|  6 <= 6 | 6 <= 6
+	// r1 overlaps r2 in a single section
+	if range1Start <= range2End && range2End <= range1End {
+		return true
+	}
+
+	return false
 }
